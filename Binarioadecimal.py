27c55ed01ee4494753f51f7524ce74530a7db105
jaiver2003 <jaiver.munoz.m@uniautonoma.edu.co>
2026-08-13 20:28:12 -0500

    mejora de binario a decimal

--- Binarioadecimal.py
+++ Binarioadecimal.py
@@ -1,10 +1,12 @@
+
 binario = input("Ingresa un número binario: ")
 
-decimal = 0
-posicion = 0
+decimal = 0     
+posicion = 0    
+
 
 for digito in reversed(binario):
-    decimal += int(digito) * (2 ** posicion)
-    posicion += 1
+    decimal += int(digito) * (2 ** posicion)   
+    posicion += 1                              
 
 print(f"Decimal: {decimal}")
