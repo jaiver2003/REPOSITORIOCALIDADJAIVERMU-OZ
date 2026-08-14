341867823884131d57484b347ec3d62f94dd119a
jaiver2003 <jaiver.munoz.m@uniautonoma.edu.co>
2026-08-13 20:26:10 -0500

    mejora decimal a binario

--- Decimalabinario.py
+++ Decimalabinario.py
@@ -1,12 +1,19 @@
+
 numero = int(input("Ingresa un número decimal: "))
+
 
 if numero == 0:
     print("Binario: 0")
 else:
-    binario = ""
-    n = numero
+    binario = ""   
+    n = numero      
+
+    
     while n > 0:
-        residuo = n % 2
-        binario = str(residuo) + binario
-        n = n // 2
+        residuo = n % 2       
+        binario = str(residuo) + binario   
+        n = n // 2            
+
+
     print(f"Binario: {binario}")
+    
